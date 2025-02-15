aa74cc0d222299924dd3b3b503872eb3d9ac647d
aikdp <kdprasad028@gmail.com>
2025-02-15 17:26:21 +0530

    Shell practice

--- datatypes/02-datatypes.sh
+++ datatypes/02-datatypes.sh
@@ -3,8 +3,12 @@
 DATE=$(date +%F)
 date --date="feb 27"
 
-FEB=$(date --date="feb 27")
+FEB=$(date --date="feb 27") #this is command inside shell script
+
+TODAY=$(date "+%d-%m-%Y")   #today date in d/m/y format
 
 echo "Today date is:$DATE"
 
 echo "Feb 27th day is: $FEB"
+
+echo "Today date is: $TODAY"
