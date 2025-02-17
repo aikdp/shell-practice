478fc661903b05e152d76976fb0217ae0eb4c3ee
aikdp <kdprasad028@gmail.com>
2025-02-17 12:51:51 +0530

    backend shell

--- backend-shell/01-backend.sh
+++ backend-shell/01-backend.sh
@@ -18,6 +18,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then
         echo -e "$2 is: $R FAILED $N"
+        exit 1
     else
         echo -e "$2 is: $G SUCCESS $N"
     fi
@@ -27,6 +28,7 @@ ROOT(){
     if [ $USERID -ne 0 ]
     then
         echo -e "$R Pease run script with ROOT PREVILEGES. Example: sh $0 $N"
+        exit 1
     fi
 }
 ROOT
